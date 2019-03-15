78d1a1a24a7ab347e55ed8ca66a782b70404618e
Andrés <jvandres247@gmail.com>
2019-03-15 15:27:16 -0600

    Subida final de prueba para pagos en linea

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,27 @@
 const { Router } = require('express');
 const router = Router();
+const stripe = require('stripe')('sk_test_sWV8CoN7Zcc7WsMmTvvVnato');
 
 router.get('/',(req, res) =>{
     res.render('index');
 })
+
+router.post('/checkout', async(req, res) =>{
+     console.log(req.body);
+     const customer = await stripe.customers.create({
+         email: req.body.stripeEmail,
+         source: req.body.stripeToken
+     });
+     const charge = await stripe.charges.create({
+         amount: '299999',
+         currency: 'mxn',
+         customer: customer.id,
+         description: 'Audifonos earphods'
+     });
+     console.log(charge.id);
+     //Respuesta final para el usuario
+     res.render('download');
+
+});
 
 module.exports = router;
