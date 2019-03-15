a6742e3ec3cc6def0e0d712bc69918456158e88e
Andrés <jvandres247@gmail.com>
2019-03-14 18:24:24 -0600

    Se añaden los links para estilos e iconos, así como una imagen de prueba

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
-
+const exphbs = require('express-handlebars');
+const path = require('path');
 //Initializations
 const app = express();
+
+//Settings
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//Middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Routes
+app.use(require('./routes/index'));
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Start server
 app.listen(3000, () => {
